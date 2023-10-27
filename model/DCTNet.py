a8e41e014ef54d0a6eb9afb6ca036eb6930bf80b
MouAo <74164019+WindMirror1108@users.noreply.github.com>
2023-10-27 12:53:55 +0800

    Update DCTNet.py

--- model/DCTNet.py
+++ model/DCTNet.py
@@ -155,22 +155,6 @@ class UIM(nn.Module):
         return interactive_out
 
 
-# class ChannelAttention(nn.Module):
-#     def __init__(self, in_planes, ratio=8):
-#         super(ChannelAttention, self).__init__()
-#         # 最大池化
-#         self.max_pool = nn.AdaptiveMaxPool2d(1)
-
-#         self.fc1 = nn.Conv2d(in_planes, in_planes // ratio, 1, bias=False)
-#         self.relu1 = nn.ReLU()
-#         self.fc2 = nn.Conv2d(in_planes // ratio, in_planes, 1, bias=False)
-
-#         self.sigmoid = nn.Sigmoid()
-
-#     def forward(self, x):
-#         max_out = self.fc2(self.relu1(self.fc1(self.max_pool(x))))
-#         out = max_out
-#         return self.sigmoid(out)
 
 # CoA
 class h_sigmoid(nn.Module):
